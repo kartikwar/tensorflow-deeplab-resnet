222aebeb7c4bcba880ff398bc42378e7f158f026
Kartik <kartik@vinojs-MacBook-Pro.local>
2019-07-25 15:30:29 +0530

    training setup

--- deeplab_resnet/image_reader.py
+++ deeplab_resnet/image_reader.py
@@ -84,7 +84,8 @@ def read_labeled_image_list(data_dir, data_list):
     masks = []
     for line in f:
         try:
-            image, mask = line.strip("\n").split(' ')
+            image, mask = line.strip("\n").split(' $$ ')
+            temp = 0
         except ValueError: # Adhoc for test.
             image = mask = line.strip("\n")
         images.append(data_dir + image)
